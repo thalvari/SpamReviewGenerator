d4652cbfc095787910c4c9a3a0066f68800bca92
thalvari <tuomas.halvari@helsinki.fi>
2018-09-25 13:15:48 +0300

    Added more folders.

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import pandas as pd
 df = pd.read_csv('data/7282_with_textcat_languagetags.csv', index_col=0)
 df.columns = df.columns.str.replace('.', "_")
 df = df.query('language_textcat == ["english", "scots"]')
-print(df)
+print('\n'.join('{}: {}'.format(i, df.query('reviews_rating == {}'.format(i)).shape[0]) for i in range(1, 6)))
