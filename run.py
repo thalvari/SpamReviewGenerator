d9d87b2db5a45ad432c48233757e05f37443d9c4
thalvari <tuomas.halvari@helsinki.fi>
2018-09-25 03:08:49 +0300

    Added a dataset with hotel reviews.

--- run.py
+++ run.py
@@ -1,1 +1,6 @@
-print("Hello world!")
+import pandas as pd
+
+df = pd.read_csv('data/7282_with_textcat_languagetags.csv', index_col=0)
+df.columns = df.columns.str.replace('.', "_")
+df = df.query('language_textcat == ["english", "scots"]')
+print(df)
